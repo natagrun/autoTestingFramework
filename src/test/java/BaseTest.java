2af912f4c6a4b38c93254adcebd8feb0a1383262
Nata <ngrunskaya1994@gmail.com>
2024-08-27 10:54:51 +0300

    experiment

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,12 @@
 import org.apache.logging.log4j.Logger;
+import org.junit.Test;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.utils.JSONHandler;
 import org.utils.LoggerSingleton;
 import org.utils.driver.DriverFactory;
 
+import static org.testng.AssertJUnit.assertTrue;
 
 public class BaseTest {
     public static JSONHandler jsonConfigHandler = new JSONHandler("src/main/java/org/utils/config.json");
@@ -15,7 +17,18 @@ public class BaseTest {
     public void setup() {
         logger.info("setting driver up");
         DriverFactory.getDriver().manage().window().maximize();
-        DriverFactory.getDriver().get(jsonConfigHandler.getValue("start page"));
+        DriverFactory.getDriver().get(System.getProperty("startPage"));
+    }
+    
+    @Test
+    public void testTest(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        assertTrue(true);
+
     }
 
     @AfterTest

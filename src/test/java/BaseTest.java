6dea90338a20dec8d8c62df98b95f8640f0b6826
Nata <ngrunskaya1994@gmail.com>
2024-08-27 11:17:32 +0300

    experiment

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,7 @@
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import org.utils.JSONHandler;
 import org.utils.LoggerSingleton;
 import org.utils.driver.DriverFactory;
@@ -15,7 +16,15 @@ public class BaseTest {
     public void setup() {
         logger.info("setting driver up");
         DriverFactory.getDriver().manage().window().maximize();
-        DriverFactory.getDriver().get(jsonConfigHandler.getValue("start page"));
+        DriverFactory.getDriver().get(System.getProperty("startPage"));
+    }
+    @Test
+    public void testTest(){
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @AfterTest
